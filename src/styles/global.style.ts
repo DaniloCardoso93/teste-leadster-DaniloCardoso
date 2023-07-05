249089d9262df1f2214397aeea8688b4a51dbac5
Danilo Cardoso <levitano@hotmail.com>
2023-07-05 13:52:44 -0300

    feat: create banner responsive

--- src/styles/global.style.ts
+++ src/styles/global.style.ts
@@ -12,25 +12,6 @@ export const theme = extendTheme({
         fontSize: "1.6rem",
         lineHeight: "3rem",
       },
-      ".ourTeam-iconWrapper-desk": {
-        border: "2px solid",
-        borderColor: "whiteFixed",
-        borderRadius: "11px",
-        p: "1.5rem",
-        img: {
-          width: "4.6rem",
-        },
-      },
-      ".ourTeam-card": {
-        as: "button",
-        display: "inline-block",
-        width: "23rem",
-        background: "whiteFixed",
-        transitionDuration: "0.2s",
-        ":focus": {
-          transform: "scale(1.3)",
-        },
-      },
     },
   },
 });
